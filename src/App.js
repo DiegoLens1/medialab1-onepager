5a2a83973122a758c380aee57c9a953be4529c91
DiegoLens1 <d.lens2712@gmail.com>
2024-03-04 11:16:15 +0100

    things

--- src/App.js
+++ src/App.js
@@ -1,48 +1,138 @@
-import './App.css';
-import placeholder from './placeholder.webp'
+import "./App.css";
+import placeholder from "./placeholder.webp";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Wie zijn wij?
-      </h1>
-      <p>
-        Mensen
-      </p>
+      <header className="header">
+        <div className="headerText">
+          <h1>Valpreventie Team 4</h1>
+        </div>
+      </header>
+      <div className="introWrapper">
+        <h2>Wie zijn wij?</h2>
+        <p>Mark, Robin, Quinten, Aiden en Diego</p>
+        <p>
+          Wij zijn een team van studenten aan de hoge school Rotterdam. Ons doel
+          is om fysiotherapeuten te helpen met het visualiseren van de data die
+          uit een valpreventie test komt.
+        </p>
+        <p></p>
+      </div>
       <div className="prototypeWrapper">
         <div className="first-prototype prototype">
-        <div className="imageWrapper">
-            <img src={placeholder}/>
-            <img src={placeholder}/>
+          <div className="imageWrapper">
+            <img src={placeholder} />
+            <img src={placeholder} />
           </div>
-          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
-
-Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
-
-Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+          <div className="descriptionWrapper">
+            <h2>prototype een</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse volutpat ornare est, sed volutpat ipsum vulputate
+              eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien
+              ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique
+              hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec
+              sem sodales efficitur ac non justo. Praesent nec massa maximus,
+              iaculis ante vitae, commodo sem.
+            </p>
+            <p>
+              Duis convallis turpis et elit finibus scelerisque. Duis volutpat
+              ipsum ac semper tincidunt. Integer accumsan condimentum viverra.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac
+              felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae
+              laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros
+              nibh tristique urna, at condimentum quam ex non sem. Duis sed
+              libero quam. Pellentesque eleifend, augue sed interdum tempus,
+              justo tellus auctor tellus, condimentum tincidunt risus ligula ac
+              augue. Etiam ac tincidunt nisi.
+            </p>
+            <p>
+              Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae
+              faucibus at, facilisis non ante. Pellentesque aliquet pulvinar
+              nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus
+              et euismod magna. Suspendisse quam dui, ornare in nulla ac,
+              imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel
+              tincidunt turpis. In iaculis ipsum vel orci sodales porttitor.
+              Fusce lorem nisi, convallis id massa congue, tristique.
+            </p>
+          </div>
         </div>
         <div className="second-prototype prototype">
-        <div className="imageWrapper">
-            <img src={placeholder}/>
-            <img src={placeholder}/>
+          <div className="imageWrapper">
+            <img src={placeholder} />
+            <img src={placeholder} />
           </div>
-          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
-
-Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
-
-Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+          <div className="descriptionWrapper">
+            <h2>prototype twee</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse volutpat ornare est, sed volutpat ipsum vulputate
+              eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien
+              ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique
+              hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec
+              sem sodales efficitur ac non justo. Praesent nec massa maximus,
+              iaculis ante vitae, commodo sem.
+            </p>
+            <p>
+              Duis convallis turpis et elit finibus scelerisque. Duis volutpat
+              ipsum ac semper tincidunt. Integer accumsan condimentum viverra.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac
+              felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae
+              laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros
+              nibh tristique urna, at condimentum quam ex non sem. Duis sed
+              libero quam. Pellentesque eleifend, augue sed interdum tempus,
+              justo tellus auctor tellus, condimentum tincidunt risus ligula ac
+              augue. Etiam ac tincidunt nisi.
+            </p>
+            <p>
+              Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae
+              faucibus at, facilisis non ante. Pellentesque aliquet pulvinar
+              nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus
+              et euismod magna. Suspendisse quam dui, ornare in nulla ac,
+              imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel
+              tincidunt turpis. In iaculis ipsum vel orci sodales porttitor.
+              Fusce lorem nisi, convallis id massa congue, tristique.
+            </p>
+          </div>
         </div>
         <div className="third-prototype prototype">
           <div className="imageWrapper">
-            <img src={placeholder}/>
-            <img src={placeholder}/>
+            <img src={placeholder} />
+            <img src={placeholder} />
           </div>
-          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
-
-Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
-
-Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+          <div className="descriptionWrapper">
+            <h2>prototype drie</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse volutpat ornare est, sed volutpat ipsum vulputate
+              eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien
+              ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique
+              hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec
+              sem sodales efficitur ac non justo. Praesent nec massa maximus,
+              iaculis ante vitae, commodo sem.
+            </p>
+            <p>
+              Duis convallis turpis et elit finibus scelerisque. Duis volutpat
+              ipsum ac semper tincidunt. Integer accumsan condimentum viverra.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac
+              felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae
+              laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros
+              nibh tristique urna, at condimentum quam ex non sem. Duis sed
+              libero quam. Pellentesque eleifend, augue sed interdum tempus,
+              justo tellus auctor tellus, condimentum tincidunt risus ligula ac
+              augue. Etiam ac tincidunt nisi.
+            </p>
+            <p>
+              Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae
+              faucibus at, facilisis non ante. Pellentesque aliquet pulvinar
+              nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus
+              et euismod magna. Suspendisse quam dui, ornare in nulla ac,
+              imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel
+              tincidunt turpis. In iaculis ipsum vel orci sodales porttitor.
+              Fusce lorem nisi, convallis id massa congue, tristique.
+            </p>
+          </div>
         </div>
       </div>
     </div>
