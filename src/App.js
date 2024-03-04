ceabf430ea073097dda8886bf1d202785566406d
Diego <d.lens2712@gmail.com>
2024-03-04 02:52:20 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import placeholder from './placeholder.webp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="first-prototype prototype">
-        test
-      </div>
-      <div className="second-prototype prototype">
-        test
-      </div>
-      <div className="third-prototype prototype">
-        test
+      <h1>
+        Wie zijn wij?
+      </h1>
+      <p>
+        Mensen
+      </p>
+      <div className="prototypeWrapper">
+        <div className="first-prototype prototype">
+        <div className="imageWrapper">
+            <img src={placeholder}/>
+            <img src={placeholder}/>
+          </div>
+          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
+
+Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
+
+Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+        </div>
+        <div className="second-prototype prototype">
+        <div className="imageWrapper">
+            <img src={placeholder}/>
+            <img src={placeholder}/>
+          </div>
+          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
+
+Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
+
+Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+        </div>
+        <div className="third-prototype prototype">
+          <div className="imageWrapper">
+            <img src={placeholder}/>
+            <img src={placeholder}/>
+          </div>
+          <div className="descriptionWrapper">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ornare est, sed volutpat ipsum vulputate eget. Vivamus vehicula tellus in rutrum ultrices. Nullam et sapien ac dolor suscipit imperdiet ut eget velit. Suspendisse tristique hendrerit sapien tincidunt posuere. Vestibulum aliquet tellus nec sem sodales efficitur ac non justo. Praesent nec massa maximus, iaculis ante vitae, commodo sem. Duis convallis turpis et elit finibus scelerisque. Duis volutpat ipsum ac semper tincidunt.
+
+Integer accumsan condimentum viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac felis aliquam, blandit urna quis, rutrum dolor. Nullam vitae laoreet tellus. Fusce fringilla, tortor nec efficitur auctor, eros nibh tristique urna, at condimentum quam ex non sem. Duis sed libero quam. Pellentesque eleifend, augue sed interdum tempus, justo tellus auctor tellus, condimentum tincidunt risus ligula ac augue.
+
+Etiam ac tincidunt nisi. Sed at vestibulum tortor. Vestibulum justo augue, tempor vitae faucibus at, facilisis non ante. Pellentesque aliquet pulvinar nibh nec auctor. Donec vel urna in magna laoreet lacinia. Vivamus et euismod magna. Suspendisse quam dui, ornare in nulla ac, imperdiet consequat ligula. Maecenas sed dolor mi. Aliquam vel tincidunt turpis. In iaculis ipsum vel orci sodales porttitor. Fusce lorem nisi, convallis id massa congue, tristique.</div>
+        </div>
       </div>
     </div>
   );
